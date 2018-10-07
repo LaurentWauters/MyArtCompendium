2ac82de69fbf3c16d4d868eb209e51e7afd04696
V W <ltwauters@bigs-macbook-pro.local>
2018-10-07 14:07:11 +0200

    Initial Commit

---
+++ MyArtCompendium/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyArtCompendium
+//
+//  Created by Big Boi on 07/10/2018.
+//  Copyright © 2018 Big Boi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
